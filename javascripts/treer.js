05a1c39c3bf9772eda10b9c889a225e18d2e73ef
Korvin Szanto <korvinszanto@gmail.com>
2013-08-09 19:46:51 -0700

    use jQuery:

--- javascripts/treer.js
+++ javascripts/treer.js
@@ -265,5 +265,12 @@ Node.prototype = {
     var cat = Category(this);
     return cat.renderCanvas();
   };
+  if (window.jQuery) {
+    window.jQuery.fn.treer = function(){
+      this.each(function(){
+        this.treer();
+      });
+    }
+  }
 
 }(window, document, undefined));
