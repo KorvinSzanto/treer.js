196d643ef1c885062e98ea3f637c8d4586e0636c
Korvin Szanto <korvinszanto@gmail.com>
2013-08-09 19:43:22 -0700

    jquery support

--- src/footer.js
+++ src/footer.js
@@ -3,5 +3,12 @@
     var cat = Category(this);
     return cat.renderCanvas();
   };
+  if (window.jQuery) {
+    window.jQuery.fn.treer = function(){
+      this.each(function(){
+        this.treer();
+      });
+    }
+  }
 
 }(window, document, undefined));
